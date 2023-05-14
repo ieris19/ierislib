baae0ca8a094ccb590d7ff8a5dd0ec5919f43be1
Ieris19 <ieris19@outlook.com>
2023-05-14 16:42:24 +0200

    Delete Annotation Processor
    The project is stale, no progress in ideas or design for too long, nothing that currently exists is functional. Thus, the decision to delete said module has been made

--- reflection/annotation-lib/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * This module is a container for all the annotation implemented by IerisLib. It contains some annotations that are used
- * by the compiler, and some that are used by the programmer. Those that are used by the compiler are processed by the
- * annotation-processor module, the rest are merely informative of intentions of the programmer.
- */
-module ierislib.reflection.annotations {
-
-	exports com.ieris19.lib.reflection.annotations;
-}
